7c371e6087a1af89245eba02c821d4f220c279c4
zhangyong <zahngyong@exiao.tech>
2023-12-20 18:15:23 +0800

    新增xml

--- xml-demo.py
+++ xml-demo.py
@@ -7,26 +7,25 @@ app = Flask(__name__)
 @app.route('/get_xml_data', methods=['GET'])
 def get_xml_data():
     # 创建XML结构
-    root = Element('people')
-    root.set('xmlns:xul', 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul')
+    root = Element('root')
 
-    person1 = SubElement(root, 'person')
-    name1 = SubElement(person1, 'name', first='george', last='bush')
-    address1 = SubElement(person1, 'address', street='1600 pennsylvania avenue', city='washington', country='usa')
-    phoneNumber1 = SubElement(person1, 'phoneNumber')
-    phoneNumber1.text = '202-456-1111'
+    # 创建第一个A元素
+    a1 = SubElement(root, 'A')
+    b1 = SubElement(a1, 'b')
+    b1.text = '1'
 
-    person2 = SubElement(root, '{http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul}person')
-    name2 = SubElement(person2, '{http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul}name', first='tony', last='blair')
-    address2 = SubElement(person2, '{http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul}address', street='10 downing street', city='london', country='uk')
-    phoneNumber2 = SubElement(person2, '{http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul}phoneNumber')
-    phoneNumber2.text = '020 7925 0918'
+    # 创建第二个A元素
+    a2 = SubElement(root, 'A')
+    b2 = SubElement(a2, 'b')
+    b2.text = '2'
 
     # 将XML格式化为字符串
-    xml_str = minidom.parseString(tostring(root)).toprettyxml(indent="  ")
+    xml_str = minidom.parseString(tostring(root)).toprettyxml(indent="  ", encoding="UTF-8")
 
     # 返回XML响应
     return Response(xml_str, content_type='text/xml')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8805, debug=True)
+
+    if __name__ == '__main__':
+        app.run(host="0.0.0.0", port=8805, debug=True)
