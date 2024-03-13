a4f1062a2ef7572acb3522f95bde8c807cf4c3b8
zhangyong <zahngyong@exiao.tech>
2024-03-13 17:51:10 +0800

    测试wechat3

--- wechat.py
+++ wechat.py
@@ -1,34 +1,40 @@
-from flask import Flask, request
+from datetime import time
+from flask import Flask, request, make_response
 import hashlib
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
-TOKEN = 'your_token'  # 替换为您在微信公众平台设置的 Token
+TOKEN = '9612ab76ba4ec146e57c63c118b02d16'  # 替换为您在微信公众平台设置的 Token
 
-def check_signature():
-    signature = request.args.get('signature', '')
-    timestamp = request.args.get('timestamp', '')
-    nonce = request.args.get('nonce', '')
-
-    tmp_arr = [TOKEN, timestamp, nonce]
-    tmp_arr.sort()
-    tmp_str = ''.join(tmp_arr)
-    tmp_str = hashlib.sha1(tmp_str.encode()).hexdigest()
-
-    if tmp_str == signature:
-        return True
-    else:
-        return False
-
-@app.route('/wechat', methods=['GET', 'POST'])
+@app.route('/wechat', methods=['POST'])
 def wechat():
-    if request.method == 'GET':
-        if check_signature():
-            return request.args.get('echostr', '')
-        else:
-            return 'Invalid Request'
-    elif request.method == 'POST':
-        # 处理微信服务器发送的消息与事件
-        return 'Success'
+    # 解析 XML 数据包
+    xml_data = request.data
+    xml_root = ET.fromstring(xml_data)
+
+    # 提取出消息类型和文本内容
+    msg_type = xml_root.find('MsgType').text
+    content = xml_root.find('Content').text
+
+    if msg_type == 'text':
+        # 返回固定值
+        reply_content = '您发送的消息是：' + content
+
+        # 组装回复消息的 XML 格式
+        reply_xml = f'''
+            <xml>
+                <ToUserName><![CDATA[{xml_root.find('FromUserName').text}]]></ToUserName>
+                <FromUserName><![CDATA[{xml_root.find('ToUserName').text}]]></FromUserName>
+                <CreateTime>{int(time.time())}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[{reply_content}]]></Content>
+            </xml>
+        '''
+
+        # 返回消息
+        response = make_response(reply_xml)
+        response.headers['Content-Type'] = 'application/xml'
+        return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
